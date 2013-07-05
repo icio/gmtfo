f419b629dc79250d5470328f74aa31299a6b4b82
Paul Scott <paul.scotto@gmail.com>
2013-07-05 16:24:06 +0100

    First try with d3 path

--- gmtfo/static/scripts/app.js
+++ gmtfo/static/scripts/app.js
@@ -5,14 +5,18 @@ requirejs.config({
 		"angular": "angular-1.0.7.min",
 		"bootstrap": "bootstrap-2.3.2.min",
 		"lessjs": "less-1.4.1.min",
-		"mapbox": "mapbox-1.2.0.min"
+		"mapbox": "mapbox-1.2.0.min",
+		"leaflet-d3": "leaflet-d3-147a943",
+		"d3": "d3-v3.min",
 	},
 	shim: {
 		// External lib setup
 		'jquery': { 'exports': '$' },
 		'bootstrap': { 'deps': ['jquery'] },
 		'angular': { 'deps': ['jquery'], 'exports': 'angular' },
+		'd3': { 'exports': 'd3' },
 		'mapbox': { 'exports': 'L' },
+		'leaflet-d3': { 'deps': ['mapbox', 'd3'], 'exports': 'L.GeoJSON.d3' },
 		
 		// Force module loading:
 		'gmtfo/main': { 'deps': [
--- gmtfo/static/scripts/gmtfo/map.js
+++ gmtfo/static/scripts/gmtfo/map.js
@@ -1,5 +1,7 @@
-define(["mapbox"], function(mapbox) {
+define(["mapbox", "leaflet-d3"], function(mapbox, d3Layer)
+{
 	mapbox = L.mapbox; // FIXME Requirejs isn't being setup correctly
+
 	return function(e, mapId)
 	{
 		var map = mapbox.map(e, mapId, {
@@ -11,47 +13,65 @@ define(["mapbox"], function(mapbox) {
 		// map.doubleClickZoom.disable();
 		// map.scrollWheelZoom.disable();
 
-		var routesLayer = mapbox.markerLayer();
-		routesLayer.addTo(map);
-		routesLayer.setGeoJSON({
-		    type: "FeatureCollection",
-		    features: [{
-		        type: "Feature",
-		        geometry: {
-		            type: "Point",
-		            coordinates: [102.0, 0.5]
-		        },
-		        properties: { }
-		    }]
-		});
+		var pathsLayer, pointsLayer;
 
 		function setRoutes(routes) {
+			if (pathsLayer) {
+				map.removeLayer(pathsLayer);
+				pathsLayer = null;
+			}
+			if (pointsLayer) {
+				map.removeLayer(pointsLayer);
+				pointsLayer = null
+			}
+
 			// Handle empty responses
 			if (!routes || !routes.length) {
 				return;
 			}
 
-			var feature = {
+			var pointsFeature = {
+				"type": "FeatureCollection",
+				"features": []
+			};
+			var pathsFeature = {
 				"type": "FeatureCollection",
 				"features": []
 			};
 			for (var r = 0, R = routes.length; r < R; r++) {
+				var pathCoordinates = [];
 				for (var p = 0, P = routes[r].length; p < P; p++) {
-					var point = routes[r][p];
+					var point = routes[r][p],
+					    coordinates = [point.lon, point.lat];
 					point.title = point.name
-					feature.features.push({
+
+					pathCoordinates.push(coordinates);
+					pointsFeature.features.push({
 						"type": "Feature",
 						"geometry": {
 							"type": "Point",
-							"coordinates": [point.lon, point.lat]
+							"coordinates": coordinates
 						},
 						"properties": point
 					});
 				}
+				pathsFeature.features.push({
+					"type": "Feature",
+					"geometry": {
+						"type": "LineString",
+						"coordinates": pathCoordinates
+					},
+					"properties": {}
+				});
 			}
 
-			console.log(feature);
-			routesLayer.setGeoJSON(feature);
+			pathsLayer = new d3Layer(pathsFeature);
+			console.log(pathsLayer, pathsFeature);
+			pathsLayer.addTo(map);
+
+			pointsLayer = mapbox.markerLayer();
+			pointsLayer.setGeoJSON(pointsFeature);
+			pointsLayer.addTo(map);
 		}
 
 		return {
